f3f792a4cc84560e810203c427d04a1f44a20c78
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2025-05-25 23:17:43 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const {City}=require('../models/index')
 
 class CityRepository{
@@ -7,6 +8,7 @@ class CityRepository{
            return city;
         }
         catch(error){
+               console.log("Something went wrong in the repository layer");
                throw error;
         }
     }
@@ -16,10 +18,34 @@ class CityRepository{
                 where:{
                     id:cityId
                 }
-            })
+            });
+            return true;
         }
         catch(error){
+               console.log("Something went wrong in the repository layer");
                throw error;
+        }
+    }
+    async updateCity(cityId,data){
+                try {
+                    const city=await City.update(data,{
+                        where:{
+                            id:cityId
+                        }
+                    })
+                    return city;
+                } catch (error) {
+                     console.log("Something went wrong in the repository layer");
+               throw error;
+                }
+    }
+    async getCity(cityId){
+        try {
+            const city =await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+             console.log("Something went wrong in the repository layer");
+             throw error;
         }
     }
 }
